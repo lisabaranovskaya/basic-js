fb9c8b30883fc067301e5013b219ac9b173926e2
Lisa Baranovskaya <liza.baranovvvskaya@gmail.com>
2020-03-19 10:54:43 +0300

    feat: solve what season task

--- src/what-season.js
+++ src/what-season.js
@@ -1,4 +1,33 @@
-module.exports = function getSeason(/* date */) {
-  throw 'Not implemented';
+module.exports = function getSeason(date) {
   // remove line with error and write your code here
+  if (arguments.length === 0) {
+    return 'Unable to determine the time of year!';
+  } else if (isNaN(date.getTime())) {
+    throw new Error();
+  }
+
+  let month = date.getMonth();
+  switch (month) {
+    case 0:
+    case 1:
+    case 11:
+      return 'winter';
+      break;
+    case 2:
+    case 3:
+    case 4:
+      return 'spring';
+      break;
+    case 5:
+    case 6:
+    case 7:
+      return 'summer';
+      break;
+    case 8:
+    case 9:
+    case 10:
+      return 'autumn';
+      break;
+  }
+
 };
