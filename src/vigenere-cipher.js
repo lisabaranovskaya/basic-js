18c39a8c02ca1fe665ac494b9c4665ad695084a6
Lisa Baranovskaya <liza.baranovvvskaya@gmail.com>
2020-03-22 23:52:57 +0300

    feat: vigenere task

--- src/vigenere-cipher.js
+++ src/vigenere-cipher.js
@@ -1,12 +1,33 @@
 class VigenereCipheringMachine {
-    encrypt() {
-        
-        
+    encrypt(message, key) {
+        key = key.toUpperCase();
+        let nonAlpha = 0;
+
+        return message.toUpperCase().split("").map((e, i) => {
+            if (e.toLowerCase() == e.toUpperCase()) {
+                nonAlpha++;
+                return e;
+            } 
+            return String.fromCharCode(
+                65 + (
+                    e.charCodeAt(0) - 65 + 
+                    key.charCodeAt((i + key.length - nonAlpha) % key.length) - 65 
+                ) % 26
+            );
+        }).join("");
     }
 
-    decrypt() {
-        
-        
+    decrypt(encryptedMessage, key) {
+        key = key.toUpperCase()
+        let nonAlpha = 0;
+
+        return encryptedMessage.toUpperCase().split("").map((e, i) => {
+            if (e.toLowerCase() == e.toUpperCase()) {
+                nonAlpha++;
+                return e;
+            } 
+            return String.fromCharCode(65 + (e.charCodeAt(0) - 65 - key.charCodeAt((i + key.length - nonAlpha) % key.length) + 65 + 26) % 26);
+        }).join("");
     }
 }
 
