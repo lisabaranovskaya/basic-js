9482712b4399634e1c9465adcb766c66738f96c5
Lisa Baranovskaya <liza.baranovvvskaya@gmail.com>
2020-03-22 18:41:56 +0300

    feat: solve chain task

--- src/simple-chain.js
+++ src/simple-chain.js
@@ -1,22 +1,34 @@
 const chainMaker = {
+  chain : [],
+  result: [],
   getLength() {
-    
+    return this.chain.length;
     // remove line with error and write your code here
   },
   addLink(value) {
-    
+    value === undefined ? this.chain.push('( )') : this.chain.push(`( ${value} )`);
+    return this;
     // remove line with error and write your code here
   },
   removeLink(position) {
-    
+    if (this.chain[position-1] === undefined || !(Number.isInteger(position)) || parseInt(position) !== position) {
+      this.chain = [];
+      throw Error;
+    } else {
+      this.chain.splice(position-1, 1);
+    }
+    return this;
     // remove line with error and write your code here
   },
   reverseChain() {
-    
+    this.chain.reverse();
+    return this;
     // remove line with error and write your code here
   },
   finishChain() {
-    
+    this.result = this.chain.join('~~');
+    this.chain = [];
+    return this.result;
     // remove line with error and write your code here
   }
 };
